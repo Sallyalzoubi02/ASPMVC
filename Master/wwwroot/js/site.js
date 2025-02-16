27d5b4fe5bbc7a5b9d0de59ac04ad7e4fd9c4bda
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-02-16 22:04:21 +0300

    Add project files.

---
+++ Master/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".animate"); // اختيار العناصر
+    const elements2 = document.querySelectorAll(".animate2"); // اختيار العناصر
+
+    setTimeout(() => {
+        elements.forEach(element => {
+            element.classList.add("visible"); // إضافة الـ class لجعل العنصر يظهر
+        });
+    }, 500); // وقت التأخير (500 مللي ثانية)
+    setTimeout(() => {
+        elements2.forEach(element => {
+            element.classList.add("visible2"); // إضافة الـ class لجعل العنصر يظهر
+        });
+    }, 500); // وقت التأخير (500 مللي ثانية)
+});
