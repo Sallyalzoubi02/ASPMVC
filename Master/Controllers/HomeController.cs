b3a10c1fa258d6144475f11c2fa14260264b6d78
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-02-17 09:21:58 +0300

    shops

--- Master/Controllers/HomeController.cs
+++ Master/Controllers/HomeController.cs
@@ -27,15 +27,7 @@ namespace Master.Controllers
         {
             return View();
         } 
-        public IActionResult Cart()
-        {
-            return View();
-        }
-
-        public IActionResult Shop()
-        {
-            return View();
-        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Master/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Master.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
